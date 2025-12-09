08e1d81f11726c9da967558cfd06ff2447b299b3
Nis5680 <nishitshah5680@gmail.com>
2025-12-09 18:25:39 +0530

    1st_change

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { email } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('forloop-app');
+  users=["Nishit", "Raj", "Sanjay", "Mohit","Kenil"];
+  students=
+  [
+    {
+      name:"Nishit",
+      age:24,
+      email:"nishit@gmail.com"
+    },
+    {
+      name:"Raj",
+      age:25,
+      email:"raj@gmail.com"
+    },
+    {
+      name:"Sanjay",
+      age:23,
+      email:"sanjay@gmail.com"
+    },
+    {
+      name:"Mohit",
+      age:26,
+      email:"mohit@gmail.com"
+    },
+    {
+      name:"Kenil",
+      age:24,
+      email:"kenil@gmail.com"
+    }
+  ]
+
+  getName(xx:string){
+    console.log(xx);
+    }
 }
